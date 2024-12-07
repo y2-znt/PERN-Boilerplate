d1498d08f64fec74ea827495de792b8bae510ea0
Yoni-Deserbaix <yonideserbaix@gmail.com>
2024-12-07 14:46:22 +0100

    feat: add Card component and update index page

--- frontend/src/pages/index.tsx
+++ frontend/src/pages/index.tsx
@@ -1,3 +1,22 @@
+import {
+  Card,
+  CardDescription,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
+
 export default function index() {
-  return <div className="text-4xl font-bold">Hello world</div>;
+  return (
+    <div className="max-w-7xl mx-auto">
+      Hello world
+      <div className="mt-12">
+        <Card className="w-1/4">
+          <CardHeader>
+            <CardTitle>Card Title</CardTitle>
+            <CardDescription>Card Description</CardDescription>
+          </CardHeader>
+        </Card>
+      </div>
+    </div>
+  );
 }
