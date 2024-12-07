2107f76bb57d0e2d5f0afa9aea95646be30af36f
Yoni-Deserbaix <yonideserbaix@gmail.com>
2024-12-07 15:50:59 +0100

    feat: integrate React Query Devtools for enhanced debugging in data fetching

--- frontend/src/pages/_app.tsx
+++ frontend/src/pages/_app.tsx
@@ -1,5 +1,6 @@
 import "@/styles/globals.css";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 import type { AppProps } from "next/app";
 
 const queryClient = new QueryClient();
@@ -8,6 +9,7 @@ export default function App({ Component, pageProps }: AppProps) {
   return (
     <QueryClientProvider client={queryClient}>
       <Component {...pageProps} />
+      <ReactQueryDevtools initialIsOpen={false} />
     </QueryClientProvider>
   );
 }
