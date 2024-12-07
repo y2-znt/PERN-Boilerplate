4ec46794589323d65250793e5e97ec0524b15581
Yoni-Deserbaix <yonideserbaix@gmail.com>
2024-12-07 10:21:26 +0100

    feat: init backend server with express & typescript

---
+++ backend/src/index.ts
@@ -0,0 +1,29 @@
+import express, { Request, Response } from "express";
+
+const app = express();
+
+// JSON
+app.use(express.json());
+
+// CORS
+app.use((req, res, next) => {
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
+  res.setHeader("Access-Control-Allow-Headers", "Content-Type");
+  next();
+});
+
+//test api
+app.get("/", (req: Request, res: Response) => {
+  try {
+    res.status(200).json({ message: "API is working" });
+  } catch (error) {
+    res.status(500).json({ message: (error as Error).message });
+  }
+});
+
+const PORT = process.env.PORT || 4000;
+
+app.listen(PORT, () => {
+  console.log(`Server is running on http://localhost:${PORT}`);
+});
