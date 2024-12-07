f615b49e87dbb842d2e7852d41aeb731f001e3e5
Yoni-Deserbaix <yonideserbaix@gmail.com>
2024-12-07 15:24:26 +0100

    feat: create Home page component and update index page to include Home

---
+++ frontend/src/pages/Home.tsx
@@ -0,0 +1,14 @@
+import UsersList from "@/components/ui/UsersList";
+
+export default function Home() {
+  return (
+    <div className="flex justify-center mx-auto">
+      <div className="mt-12 px-8">
+        <h1 className="text-4xl font-bold pb-12 text-center">
+          User Management
+        </h1>
+        <UsersList />
+      </div>
+    </div>
+  );
+}
--- frontend/src/pages/index.tsx
+++ frontend/src/pages/index.tsx
@@ -1,22 +1,9 @@
-import {
-  Card,
-  CardDescription,
-  CardHeader,
-  CardTitle,
-} from "@/components/ui/card";
+import Home from "./Home";
 
 export default function index() {
   return (
     <div className="max-w-7xl mx-auto">
-      Hello world
-      <div className="mt-12">
-        <Card className="w-1/4">
-          <CardHeader>
-            <CardTitle>Card Title</CardTitle>
-            <CardDescription>Card Description</CardDescription>
-          </CardHeader>
-        </Card>
-      </div>
+      <Home />
     </div>
   );
 }
