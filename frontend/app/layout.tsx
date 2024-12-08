5ee615c8eba158e804efb0214aadb8689ef5b9cf
Yoni-Deserbaix <yonideserbaix@gmail.com>
2024-12-08 16:17:13 +0100

    refactor: relocate TanstackProvider to the provider folder for improved structure

--- frontend/app/layout.tsx
+++ frontend/app/layout.tsx
@@ -1,7 +1,7 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
-import TanstackProvider from "./components/provider/tanstack-provider";
+import TanstackProvider from "./provider/tanstack-provider";
 
 const inter = Inter({ subsets: ["latin"] });
 
