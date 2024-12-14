41bab0b7c0f82875cb5c9ad989d5770791dcaec3
Yoni-Deserbaix <yonideserbaix@gmail.com>
2024-12-14 17:57:31 +0100

    feat: enhance login functionality in authController
    - Reimplemented the `login` function in `authController` to include user authentication logic.
    - Added user lookup by email and password validation using bcrypt.
    - Improved error handling for user not found and invalid password scenarios.
    - Updated response to include user data upon successful login.

--- backend/src/controllers/authController.ts
+++ backend/src/controllers/authController.ts
@@ -2,14 +2,6 @@ import bcrypt from "bcrypt";
 import { Request, Response } from "express";
 import prisma from "../prisma/prismaClient";
 
-export const login = async (req: Request, res: Response) => {
-  try {
-    res.status(200).json({ message: "Login successful" });
-  } catch (error) {
-    res.status(500).json({ message: (error as Error).message });
-  }
-};
-
 export const register = async (req: Request, res: Response) => {
   const { name, email, password } = req.body;
   try {
@@ -30,6 +22,29 @@ export const register = async (req: Request, res: Response) => {
       },
     });
     res.status(201).json({ message: "User created successfully", user });
+  } catch (error) {
+    res.status(500).json({ message: (error as Error).message });
+  }
+};
+
+export const login = async (req: Request, res: Response) => {
+  const { email, password } = req.body;
+
+  try {
+    const user = await prisma.user.findUnique({
+      where: { email },
+    });
+
+    if (!user) {
+      return res.status(404).json({ message: "User not found" });
+    }
+
+    const isPasswordValid = await bcrypt.compare(password, user.password);
+    if (!isPasswordValid) {
+      return res.status(401).json({ message: "Invalid password" });
+    }
+
+    res.status(200).json({ message: "Login successful", user });
   } catch (error) {
     res.status(500).json({ message: (error as Error).message });
   }
