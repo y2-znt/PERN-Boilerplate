e44f870d412bb623eaf9606422f1f106d8381e92
Yoni-Deserbaix <yonideserbaix@gmail.com>
2024-12-14 23:36:46 +0100

    refactor: enhance error handling in authentication routes
    - Wrapped `login` and `register` route handlers in try-catch blocks to improve error handling.
    - Utilized `NextFunction` to pass errors to the next middleware, ensuring consistent error management across authentication routes.

--- backend/src/routes/authRoutes.ts
+++ backend/src/routes/authRoutes.ts
@@ -1,9 +1,28 @@
-import express from "express";
+import express, { NextFunction, Request, Response } from "express";
 import { login, register } from "../controllers/authController";
 
 const router = express.Router();
 
-router.post("/auth/login", login);
-router.post("/auth/register", register);
+router.post(
+  "/auth/register",
+  async (req: Request, res: Response, next: NextFunction) => {
+    try {
+      await register(req, res);
+    } catch (error) {
+      next(error);
+    }
+  }
+);
+
+router.post(
+  "/auth/login",
+  async (req: Request, res: Response, next: NextFunction) => {
+    try {
+      await login(req, res);
+    } catch (error) {
+      next(error);
+    }
+  }
+);
 
 export default router;
