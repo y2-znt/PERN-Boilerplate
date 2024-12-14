220760307908d4660ca17971bbe8468e96b79855
Yoni-Deserbaix <yonideserbaix@gmail.com>
2024-12-14 17:57:43 +0100

    fix: change login route method from GET to POST in authRoutes.

--- backend/src/routes/authRoutes.ts
+++ backend/src/routes/authRoutes.ts
@@ -3,7 +3,7 @@ import { login, register } from "../controllers/authController";
 
 const router = express.Router();
 
-router.get("/auth/login", login);
+router.post("/auth/login", login);
 router.post("/auth/register", register);
 
 export default router;
