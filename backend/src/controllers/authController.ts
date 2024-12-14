bcc069cedc3c92846cc4ab08c949af02f4e67134
Yoni-Deserbaix <yonideserbaix@gmail.com>
2024-12-14 20:44:37 +0100

    feat: implement JWT authentication in authController
    - Added JSON Web Token (JWT) support for user login functionality.
    - Updated the `login` method to generate a token upon successful authentication.
    - Enhanced error handling in the `register` method for existing users.

--- backend/src/controllers/authController.ts
+++ backend/src/controllers/authController.ts
@@ -1,8 +1,9 @@
 import bcrypt from "bcrypt";
 import { Request, Response } from "express";
+import * as jwt from "jsonwebtoken";
 import prisma from "../prisma/prismaClient";
 
-export const register = async (req: Request, res: Response) => {
+export const register = async (req: Request, res: Response): Promise<void> => {
   const { name, email, password } = req.body;
   try {
     const existingUser = await prisma.user.findUnique({
@@ -10,7 +11,8 @@ export const register = async (req: Request, res: Response) => {
     });
 
     if (existingUser) {
-      return res.status(400).json({ message: "Email already exists" });
+      res.status(400).json({ message: "Email already exists" });
+      return;
     }
 
     const hashedPassword = await bcrypt.hash(password, 10);
@@ -27,7 +29,7 @@ export const register = async (req: Request, res: Response) => {
   }
 };
 
-export const login = async (req: Request, res: Response) => {
+export const login = async (req: Request, res: Response): Promise<void> => {
   const { email, password } = req.body;
 
   try {
@@ -36,15 +38,21 @@ export const login = async (req: Request, res: Response) => {
     });
 
     if (!user) {
-      return res.status(404).json({ message: "User not found" });
+      res.status(404).json({ message: "User not found" });
+      return;
     }
 
     const isPasswordValid = await bcrypt.compare(password, user.password);
     if (!isPasswordValid) {
-      return res.status(401).json({ message: "Invalid password" });
+      res.status(401).json({ message: "Invalid password" });
+      return;
     }
 
-    res.status(200).json({ message: "Login successful", user });
+    const token = jwt.sign({ userId: user.id }, process.env.JWT_SECRET!, {
+      expiresIn: "1h",
+    });
+
+    res.status(200).json({ message: "Login successful", user, token });
   } catch (error) {
     res.status(500).json({ message: (error as Error).message });
   }
