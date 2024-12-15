c99d69a54e070cc520c2779f65491000f53c74d5
Yoni-Deserbaix <yonideserbaix@gmail.com>
2024-12-15 13:42:05 +0100

    refactor: simplify authentication controller and routes
    - Refactored `register` and `login` methods in `authController` to utilize a dedicated service for user registration and login, improving code organization.
    - Removed bcrypt and jwt logic from the controller, delegating it to the `authService`.
    - Enhanced error handling to provide more specific error messages.
    - Streamlined route definitions in `authRoutes` by removing the custom error handling wrapper, directly using the controller functions.

--- backend/src/routes/authRoutes.ts
+++ backend/src/routes/authRoutes.ts
@@ -1,19 +1,9 @@
-import express, { NextFunction, Request, Response } from "express";
+import express from "express";
 import { login, register } from "../controllers/authController";
 
 const router = express.Router();
 
-const handleController = (controller: Function) => {
-  return async (req: Request, res: Response, next: NextFunction) => {
-    try {
-      await controller(req, res);
-    } catch (error) {
-      next(error);
-    }
-  };
-};
-
-router.post("/auth/register", handleController(register));
-router.post("/auth/login", handleController(login));
+router.post("/auth/register", register);
+router.post("/auth/login", login);
 
 export default router;
