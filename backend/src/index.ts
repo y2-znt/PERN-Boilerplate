41074b2db175afedaa9fd9a77ed7a82a91d78143
Yoni-Deserbaix <yonideserbaix@gmail.com>
2025-01-21 20:30:21 +0100

    feat: add CORS support and new API entry point
    - Introduced CORS middleware to allow cross-origin requests, configured with CLIENT_URL.
    - Created a new entry point in src/index.ts to set up the Express application and define API routes.
    - Updated package.json and package-lock.json to include 'cors' and its type definitions as dependencies.
    - Added CLIENT_URL export in config.ts for environment-based configuration.

---
+++ backend/src/index.ts
@@ -0,0 +1,31 @@
+import cors from "cors";
+import express, { Request, Response } from "express";
+import { CLIENT_URL } from "./config/config";
+import errorMiddleware from "./middlewares/errorMiddleware";
+import authRoutes from "./routes/authRoutes";
+import userRoutes from "./routes/userRoutes";
+const app = express();
+
+// CORS
+app.use(cors({ origin: CLIENT_URL }));
+
+// Error handling
+app.use(errorMiddleware);
+
+// JSON
+app.use(express.json());
+
+// Routes
+app.use("/api/users", userRoutes);
+app.use("/api/auth", authRoutes);
+
+// test api
+app.get("/", (req: Request, res: Response) => {
+  try {
+    res.status(200).json({ message: "API is working" });
+  } catch (error) {
+    res.status(500).json({ message: (error as Error).message });
+  }
+});
+
+export default app;
