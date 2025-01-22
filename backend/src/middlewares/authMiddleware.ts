5d5d894fe339fdd1d409426d9e61c0107c173acf
Yoni-Deserbaix <yonideserbaix@gmail.com>
2025-01-22 22:17:11 +0100

    refactor: update configuration imports and enhance authentication middleware
    - Changed import paths for CLIENT_URL and JWT_SECRET from config/config to config/env for better environment variable management.
    - Updated authentication middleware to use Authorization header instead of cookies for token retrieval, improving security and flexibility.
    - Added logging for received tokens to assist with debugging.

--- backend/src/middlewares/authMiddleware.ts
+++ backend/src/middlewares/authMiddleware.ts
@@ -1,20 +1,24 @@
 import { NextFunction, Response } from "express";
 import jwt from "jsonwebtoken";
-import { JWT_SECRET } from "../config/config";
-import prisma from "../prisma/prismaClient";
-import { AuthenticatedRequest } from "../utils/express";
+import { JWT_SECRET } from "../config/env";
+import prisma from "../config/prismaClient";
+import { AuthenticatedRequest } from "../types/express";
 
 const authMiddleware = async (
   req: AuthenticatedRequest,
   res: Response,
   next: NextFunction
 ): Promise<void> => {
-  const token = req.cookies.authToken;
+  const authHeader = req.headers.authorization;
 
-  if (!token) {
+  if (!authHeader || !authHeader.startsWith("Bearer ")) {
     res.status(401).json({ message: "Unauthorized: No token provided" });
     return;
   }
+
+  const token = authHeader.split(" ")[1];
+
+  console.log("Token received:", token);
 
   try {
     const decoded = jwt.verify(token, JWT_SECRET) as { userId: string };
