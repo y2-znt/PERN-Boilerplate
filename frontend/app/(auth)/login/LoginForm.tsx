c63675e906ede51d18dea813e1edda3db1b1dc73
Yoni-Deserbaix <yonideserbaix@gmail.com>
2025-01-24 19:33:14 +0100

    feat: enhance LoginForm with form handling and API integration
    - Implemented form validation using react-hook-form and Zod for email and password inputs.
    - Added login mutation with error handling and success redirection to the dashboard.
    - Improved user feedback with loading state and error messages for form validation.
    - Updated the form submission process to handle user login more effectively.

--- frontend/app/(auth)/login/LoginForm.tsx
+++ frontend/app/(auth)/login/LoginForm.tsx
@@ -1,4 +1,11 @@
+"use client";
+
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useMutation } from "@tanstack/react-query";
 import Link from "next/link";
+import { useRouter } from "next/navigation";
+import { useForm } from "react-hook-form";
+import { loginUser } from "../../api/AuthApi";
 import { Button } from "../../components/ui/button";
 import {
   Card,
@@ -8,8 +15,40 @@ import {
   CardTitle,
 } from "../../components/ui/card";
 import { InputWithLabel } from "../../components/ui/InputWithLabel";
+import { LoginFormValues, LoginSchema } from "../../schemas/authSchema";
 
 export default function LoginForm() {
+  const router = useRouter();
+  const {
+    register,
+    handleSubmit,
+    formState: { errors, isSubmitting },
+  } = useForm<LoginFormValues>({
+    resolver: zodResolver(LoginSchema),
+  });
+
+  const loginMutation = useMutation({
+    mutationFn: async (data: LoginFormValues) => {
+      console.log("Mutation exécutée avec :", data);
+      const response = await loginUser(data.email, data.password);
+      console.log("Réponse de l'API :", response);
+      return response;
+    },
+    onSuccess: (data) => {
+      console.log("Login successful!", data);
+      router.push("/dashboard");
+    },
+    onError: (error: Error) => {
+      console.error("Login failed:", error.message);
+      alert(error.message || "An error occurred during login");
+    },
+  });
+
+  const onSubmit = (data: LoginFormValues) => {
+    console.log("data", data);
+    loginMutation.mutate(data);
+  };
+
   return (
     <div className="flex flex-col gap-6">
       <Card>
@@ -20,25 +59,48 @@ export default function LoginForm() {
           </CardDescription>
         </CardHeader>
         <CardContent>
-          <form>
+          <form onSubmit={handleSubmit(onSubmit)}>
             <div className="flex flex-col gap-6">
-              <InputWithLabel
-                label="Email"
-                id="email"
-                type="email"
-                placeholder="m@example.com"
-                required
-              />
-              <InputWithLabel
-                label="Password"
-                id="password"
-                type="password"
-                required
-              />
+              <div>
+                <InputWithLabel
+                  label="Email"
+                  id="email"
+                  type="email"
+                  placeholder="m@example.com"
+                  required
+                  {...register("email")}
+                />
+                {errors.email && (
+                  <p className="m-1 text-sm text-red-500">
+                    {errors.email.message}
+                  </p>
+                )}
+              </div>
+
+              <div>
+                <InputWithLabel
+                  label="Password"
+                  id="password"
+                  type="password"
+                  required
+                  {...register("password")}
+                />
+                {errors.password && (
+                  <p className="m-1 text-sm text-red-500">
+                    {errors.password.message}
+                  </p>
+                )}
+              </div>
             </div>
-            <Button type="submit" className="mt-4 w-full">
-              Login
+
+            <Button
+              type="submit"
+              className="mt-4 w-full"
+              disabled={isSubmitting}
+            >
+              {isSubmitting ? "Logging in..." : "Login"}
             </Button>
+
             <Button variant="outline" className="mt-4 w-full">
               Login with Google
             </Button>
