635bb861501fb63d311c824b96abb6193bd0c11e
Yoni-Deserbaix <yonideserbaix@gmail.com>
2025-01-24 19:32:37 +0100

    feat: create Dashboard component for user management
    - Introduced a new Dashboard component that encapsulates UserForm and UsersList for improved organization and modularity.
    - Updated the page component to render the new Dashboard, simplifying the structure and enhancing maintainability.

---
+++ frontend/app/dashboard/Dashboard.tsx
@@ -0,0 +1,20 @@
+import UserForm from "../components/UserForm";
+import UsersList from "../components/UsersList";
+
+export default function Dashboard() {
+  return (
+    <div className="mx-auto max-w-7xl">
+      <div className="mx-auto flex justify-center">
+        <div className="mt-12 w-full px-8 md:w-1/2">
+          <h1 className="pb-12 text-center text-4xl font-bold">
+            User Management
+          </h1>
+          <div className="space-y-12">
+            <UserForm />
+            <UsersList />
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
--- frontend/app/dashboard/page.tsx
+++ frontend/app/dashboard/page.tsx
@@ -1,20 +1,5 @@
-import UserForm from "../components/UserForm";
-import UsersList from "../components/UsersList";
+import Dashboard from "./Dashboard";
 
 export default function page() {
-  return (
-    <div className="mx-auto max-w-7xl">
-      <div className="mx-auto flex justify-center">
-        <div className="mt-12 w-full px-8 md:w-1/2">
-          <h1 className="pb-12 text-center text-4xl font-bold">
-            User Management
-          </h1>
-          <div className="space-y-12">
-            <UserForm />
-            <UsersList />
-          </div>
-        </div>
-      </div>
-    </div>
-  );
+  return <Dashboard />;
 }
