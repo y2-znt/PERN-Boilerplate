0d13234d1ec957ddb09e49a46150d7bffbf3b8cf
Yoni-Deserbaix <yonideserbaix@gmail.com>
2025-01-24 20:18:59 +0100

    feat: enhance Navbar and Dashboard components with authentication logic
    - Integrated useAuthContext in Navbar to conditionally render Dashboard or Login button based on user authentication state.
    - Updated Dashboard component to include a Logout button, utilizing useAuthContext for user state management and redirecting to the home page upon logout.

--- frontend/app/components/Navbar.tsx
+++ frontend/app/components/Navbar.tsx
@@ -1,14 +1,24 @@
+"use client";
+
 import Link from "next/link";
+import { useAuthContext } from "../context/authContext";
 import { Button } from "./ui/button";
 
 export default function Navbar() {
+  const { authUser } = useAuthContext();
   return (
     <div className="flex items-center justify-between py-4">
       <div className="font-bold md:text-2xl">y2 Boilerplate</div>
       <div className="flex items-center gap-4">
-        <Link href="/dashboard">
-          <Button>Dashboard</Button>
-        </Link>
+        {authUser ? (
+          <Link href="/dashboard">
+            <Button>Dashboard</Button>
+          </Link>
+        ) : (
+          <Link href="/login">
+            <Button>Login</Button>
+          </Link>
+        )}
       </div>
     </div>
   );
--- frontend/app/dashboard/Dashboard.tsx
+++ frontend/app/dashboard/Dashboard.tsx
@@ -1,10 +1,36 @@
+"use client";
+
+import { useRouter } from "next/navigation";
+import { logoutUser } from "../api/AuthApi";
+import { Button } from "../components/ui/button";
 import UserForm from "../components/UserForm";
 import UsersList from "../components/UsersList";
+import { useAuthContext } from "../context/authContext";
 
 export default function Dashboard() {
+  const router = useRouter();
+  const { setAuthUser } = useAuthContext();
+
+  const handleLogout = async () => {
+    try {
+      await logoutUser();
+      setAuthUser(null);
+      router.push("/");
+    } catch (error) {
+      console.error("Logout failed:", error);
+    }
+  };
+
   return (
     <div className="mx-auto max-w-7xl">
-      <div className="mx-auto flex justify-center">
+      <div className="relative mx-auto flex justify-center">
+        <Button
+          onClick={handleLogout}
+          className="absolute right-8 top-8"
+          variant="outline"
+        >
+          Logout
+        </Button>
         <div className="mt-12 w-full px-8 md:w-1/2">
           <h1 className="pb-12 text-center text-4xl font-bold">
             User Management
