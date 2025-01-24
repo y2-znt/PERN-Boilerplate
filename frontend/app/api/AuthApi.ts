b565b164682817f9b8c8bdb44d573443af299e8e
Yoni-Deserbaix <yonideserbaix@gmail.com>
2025-01-24 19:17:38 +0100

    feat: implement authentication API functions for user registration, login, and logout
    - Added AuthApi.ts file containing functions for user registration, login, and logout.
    - Implemented error handling and token storage in localStorage for successful authentication.
    - Integrated API calls to the backend for user authentication processes.

---
+++ frontend/app/api/AuthApi.ts
@@ -0,0 +1,78 @@
+import { API_BASE_URL } from "../utils/apiClient";
+
+export const registerUser = async (
+  name: string,
+  email: string,
+  password: string,
+  confirmPassword: string,
+) => {
+  try {
+    const response = await fetch(`${API_BASE_URL}/auth/register`, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({ name, email, password, confirmPassword }),
+    });
+
+    if (response.ok) {
+      console.log("Registration successful");
+      const data = await response.json();
+      localStorage.setItem("token", data.token);
+      return data;
+    } else {
+      const errorData = await response.json();
+      throw new Error(errorData.message || "Error during registration");
+    }
+  } catch (error) {
+    console.error("Registration error:", error);
+    throw error;
+  }
+};
+
+export const loginUser = async (email: string, password: string) => {
+  try {
+    const response = await fetch(`${API_BASE_URL}/auth/login`, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({ email, password }),
+    });
+
+    if (response.ok) {
+      console.log("Login successful");
+      const data = await response.json();
+      localStorage.setItem("token", data.token);
+      return data;
+    } else {
+      const errorData = await response.json();
+      throw new Error(errorData.message || "Error during login");
+    }
+  } catch (error) {
+    console.error("Login error:", error);
+    throw error;
+  }
+};
+
+export const logoutUser = async () => {
+  try {
+    const response = await fetch(`${API_BASE_URL}/auth/logout`, {
+      method: "POST",
+      headers: {
+        Authorization: `Bearer ${localStorage.getItem("token")}`,
+      },
+    });
+
+    if (response.ok) {
+      console.log("Logout successful");
+      localStorage.removeItem("token");
+    } else {
+      const errorData = await response.json();
+      throw new Error(errorData.message || "Error during logout");
+    }
+  } catch (error) {
+    console.error("Logout error:", error);
+    throw error;
+  }
+};
